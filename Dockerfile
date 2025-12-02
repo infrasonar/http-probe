3159384731030fdc690343f78d854e0ce730cdc1
Jeroen van der Heijden <jeroen@cesbit.com>
2025-12-02 12:36:32 +0100

    insecure openssl

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,17 @@
 FROM ghcr.io/infrasonar/python:3.12.9
+
+ENV OPENSSL_CONF_PATH=/etc/ssl/openssl.cnf
+RUN if [ -f "$OPENSSL_CONF_PATH" ]; then \
+        cp "$OPENSSL_CONF_PATH" "$OPENSSL_CONF_PATH.bak" && \
+        \
+        # Use sed to add 'CipherString = DEFAULT@SECLEVEL=0' to the main configuration section.
+        # This targets the [default_sect] or the main body of the config.
+        # This tells OpenSSL to drop all security restrictions.
+        sed -i '/\[default_sect\]/a CipherString = DEFAULT@SECLEVEL=0' "$OPENSSL_CONF_PATH" \
+    ; else \
+        echo "Error: openssl.cnf not found at $OPENSSL_CONF_PATH" && exit 1 \
+    ; fi
+
 ADD . /code
 WORKDIR /code
 RUN pip install --no-cache-dir -r requirements.txt
--- lib/connector.py
+++ lib/connector.py
@@ -1,34 +1,18 @@
 from typing import Optional
-import ssl
 import aiohttp
 import asyncio
 
 
-# Allow unsafe legacy renegotiation when verify SSL is disabled
-SSL_OP_NO_UNSAFE_LEGACY_RENEGOTIATION = 0x00040000
-SSL_CONTEXT_UNSAFE_NO_CHECK = ssl.create_default_context()
-SSL_CONTEXT_UNSAFE_NO_CHECK.options &= ~SSL_OP_NO_UNSAFE_LEGACY_RENEGOTIATION
-SSL_CONTEXT_UNSAFE_NO_CHECK.check_hostname = False
-SSL_CONTEXT_UNSAFE_NO_CHECK.verify_mode = ssl.CERT_NONE
-SSL_CONTEXT_UNSAFE_NO_CHECK.minimum_version = ssl.TLSVersion.TLSv1
-
-
 def get_connector(
-            verify_ssl: bool,
             loop: Optional[asyncio.AbstractEventLoop] = None
         ) -> aiohttp.TCPConnector:
     if loop is None:
         loop = asyncio.get_running_loop()
-
-    ssl_context: bool | ssl.SSLContext = True
-    if verify_ssl is False:
-        ssl_context = SSL_CONTEXT_UNSAFE_NO_CHECK
 
     return aiohttp.TCPConnector(
         limit=100,  # 100 is default
         use_dns_cache=False,
         enable_cleanup_closed=True,
         force_close=True,
-        ssl=ssl_context,
         loop=loop,
     )
